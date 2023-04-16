f50d23554fa700e8849f2d1ac10a096337edb521
kajenk <86421060+kajenk@users.noreply.github.com>
2023-04-16 06:46:49 -0400

    add catalouge

--- catalogue.sh
+++ catalogue.sh
@@ -1,11 +1,11 @@
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
+cp catalogue.service /etc/systemd/system/catalogue.service
 useradd roboshop
 mkdir /app 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
 cd /app 
 unzip /tmp/catalogue.zip
 npm install
-cp catalogue.service /etc/systemd/system/catalogue.service
 systemctl enable catalogue 
 systemctl start catalogue
