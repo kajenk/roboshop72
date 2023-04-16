ca7041a14a0dd76db3dc9a7af9daed5a809b23fa
kajenk <86421060+kajenk@users.noreply.github.com>
2023-04-16 07:06:27 -0400

    added radis

--- radis.sh
+++ radis.sh
@@ -1,7 +1,7 @@
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 dnf module enable redis:remi-6.2 -y
 yum install redis -y 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/radis.conf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf
 sed -i 's/127.0.0.1/0.0.0.0/g'  /etc/redis/redis.conf
 systemctl enable redis 
 systemctl start redis 
